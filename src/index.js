c5beb74dbf23ca13a757fb33c5105e4d1f24af61
CNR <narayana.cheerla@gmail.com>
2024-02-18 11:35:51 +0530

    partial completion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {WebhookClient, Suggestion} = require("dialogflow-fulfillment");
+const {WebhookClient} = require("dialogflow-fulfillment");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -9,14 +9,67 @@ app.get('/', (req, res) => res.send("online"));
 app.post('/dialogflow', express.json(), (request, response) => {
     const agent = new WebhookClient({request, response});
     
-    function welcome() {
-        agent.add("Hi, Welcome to ABC mutual fund service. You can ask about ");
-        agent.add(new Suggestion("Portfolio valuation"));
-        agent.add(new Suggestion("Fund explorer"));
-        agent.add(new Suggestion("Transaction history"));
+    function fundExplorerIntent() {
+        agent.add("Here are fund categories you can select to view.");
     }
+
+    function fundsListIntent(){
+        console.log(JSON.stringify(agent.parameters));
+        agent.add(`
+        To select ${agent.parameters.category} fund from the below option(s), enter option number
+
+        Enter 1 ABC Overnight Fund
+        Enter 2 ABC Liquid Fund
+        Enter 3 AbC Saving Fund
+        `);
+    }
+
+    function fundExplorerFundsListFund(){
+        console.log("In fund Explorer FundsListFund intent ",JSON.stringify(agent.parameters));
+        if(!agent.parameters.fund){
+            agent.add("Please select fund");
+            return;
+        }
+        agent.add(`${agent.parameters.fund}
+        The investment objective of the scheme is to provide returns thsclosely correspond the details.
+        `);
+        agent.add(`
+            -Invest
+            -Main menu
+        `);
+    }
+
+    function serviceIntent() {
+        console.log(agent.parameters.service);
+        if(agent.parameters.service === "Fund Explorer") {
+            agent.setFollowupEvent("FundExplorerEvent");
+        }else{
+            agent.setFollowupEvent("MobileNumberEvent")
+        }
+        agent.add(`Hi, welcome to service indent from webhook. You have selected ${agent.parameters.service}`);
+    }
+
+    function mainMenuIntent(){
+        agent.setFollowupEvent("Welcome");
+    }
+
+    function investIntent(){
+        agent.setFollowupEvent("MobileNumberEvent")
+    }
+
+    function mobileNumberIntent(){
+        console.log(JSON.stringify(agent.contexts));
+        agent.add(`Awesome ${agent.parameters["phone-number"]}`);
+    }
+
     let intentMap = new Map();
-    intentMap.set("Default Welcome Intent", welcome);
+    intentMap.set("ServiceIntent", serviceIntent);
+    intentMap.set("FundExplorer", fundExplorerIntent);
+    intentMap.set("FundExplorer-FundsList",fundsListIntent);
+    intentMap.set("FundExplorer-FundsList-Fund", fundExplorerFundsListFund);
+    intentMap.set("MainMenuIntent", mainMenuIntent);
+    intentMap.set("InvestIntent", investIntent);
+    intentMap.set("MobileNumberIntent - custom",mobileNumberIntent);
     agent.handleRequest(intentMap);
 });
 
